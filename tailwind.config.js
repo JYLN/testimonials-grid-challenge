c8af4a06fbf93d24a455c6bdf108cb4581d618ee
Jaylen Baxter <14062951+JYLN@users.noreply.github.com>
2024-11-08 20:20:30 -0700

    style: :lipstick: update global styles and tailwind config with custom font and colors
    - Install `@fontsource/barlow-semi-condensed`
    - Import medium and semibold weights in `styles.css`
    - Update tailwind config with color palette from design
    - Extend tailwind config with Barlow Semi Condensed font

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,22 @@
 module.exports = {
   content: ["./src/**/*.html"],
   theme: {
-    extend: {},
+    colors: {
+      darkblackishblue: "#19202D",
+      darkgrayishblue: "#48556A",
+      lightgrayishblue: "#ECF2F8",
+      primary: "#CFCFCF",
+      white: "#FFFFFF",
+      violet: {
+        DEFAULT: "#733FC8",
+        light: "#A775F1",
+      },
+    },
+    extend: {
+      fontFamily: {
+        barlow: ["Barlow Semi Condensed", "sans-serif"],
+      },
+    },
   },
   plugins: [],
 };
